62748f816c1b048680f2d9fc3640e37145491d91
mrkorben <damien.issartel@gmail.com>
2021-02-17 16:34:18 +0100

    Delete server directory

--- server/javascripts/services/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-exports.SellerService = require('./seller')
-exports.OrderService = require('./order')
-exports.Reduction = require('./reduction')
-exports.Dispatcher = require('./dispatcher').Dispatcher
-exports.BadRequest = require('./dispatcher').BadRequest
-exports.SellerCashUpdater = require('./dispatcher').SellerCashUpdater
